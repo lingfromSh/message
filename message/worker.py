47cdaa1660459e86ad04f858d40e0713fb4d61b1
lingfromSh <541023490@qq.com>
2024-01-09 17:00:11 +0800

    feat: replace redis broker with aio pika broker

--- message/worker.py
+++ message/worker.py
@@ -1,7 +1,22 @@
 # Third Party Library
 import taskiq_fastapi
-from taskiq_redis import ListQueueBroker
+from dependency_injector.containers import DeclarativeContainer
+from dependency_injector.providers import Configuration
+from message.common.constants import SETTINGS_YAML
+from taskiq_aio_pika import AioPikaBroker
 
-broker = ListQueueBroker("redis://localhost:6379/")
+
+class DependencyContainer(DeclarativeContainer):
+    config = Configuration()
+
+
+dependency = DependencyContainer()
+dependency.config.from_yaml(SETTINGS_YAML)
+
+
+broker = AioPikaBroker(
+    url=dependency.config.queue.dsn(),
+    qos=dependency.config.queue.prefecth_count(),
+)
 
 taskiq_fastapi.init(broker, "message.main:app")
